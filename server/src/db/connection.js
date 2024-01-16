72ab944fa9f3d664908a5224593d5902a72a0e33
berzeQ <artista.berzek@gmail.com>
2024-01-16 07:11:02 +0545

    add env

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,10 +1,11 @@
 const postgresql = require("pg");
 const { Pool } = postgresql;
+require("dotenv").config();
 
 const pool = new Pool({
   user: "postgres",
-  database: "postgres",
-  password: "postgres",
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   host: "127.0.0.1",
   port: 5432,
 });
@@ -18,8 +19,28 @@ const query = async (...args) => {
     client.release();
   }
 };
+const initializeDatabase = async () => {
+  try {
+    // Create "users" table if not exists
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        phone_number BIGINT,
+
+      )
+    `);
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.error("Error initializing database", error);
+  }
+};
 
 module.exports = {
   pool,
   query,
+  initializeDatabase,
 };
