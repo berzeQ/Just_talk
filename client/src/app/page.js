1614f3c0d0a71e38ab2a82bc5dbd742754d9109c
berzeQ <artista.berzek@gmail.com>
2024-01-29 06:54:27 +0545

    ui chnges

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Navbar from "../components/navbar/page";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import {
   Tabs,
   Tab,
@@ -20,8 +20,8 @@ import MainBody from "../components/mainbody/page";
 export default function Home() {
   const { username } = useSelector((state) => state.user);
   const [cookies] = useCookies(["myCookie"]);
+  const [mounted, setMounted] = useState(false);
 
-  const [selected, setSelected] = useState("login");
   // const [token, setToken] = useState(document.cookie.split("=")[1]);
   useEffect(() => {
     console.log(cookies.myCookie);
@@ -30,30 +30,32 @@ export default function Home() {
     <div className="bg-gradient-to-r from-black via-gray-700 to-black h-screen  flex flex-col gap-14 ">
       <Navbar />
 
-      {cookies.myCookie ? (
-        <MainBody />
-      ) : (
-        <div className="flex flex-col w-2/5 h-4/5 bg-gray-900 self-center justify-evenly">
-          <Card className=" dark max-w-full w-full h-full">
-            <CardBody className="overflow-hidden">
-              <Tabs
-                fullWidth
-                size="lg"
-                aria-label="Tabs form"
-                selectedKey={selected}
-                onSelectionChange={setSelected}
-              >
-                <Tab key="login" title="Login" className="py-8 text-2xl">
-                  <Login setSelected={setSelected} />
-                </Tab>
-                <Tab key="sign-up" title="Register" className="py-8 text-2xl">
-                  <Register setSelected={setSelected} />
-                </Tab>
-              </Tabs>
-            </CardBody>
-          </Card>
-        </div>
-      )}
+      <Suspense fallback={<div className="text-white">Loading...</div>}>
+        {cookies.myCookie ? (
+          <MainBody className="w-5/6" />
+        ) : (
+          <div className="flex flex-col w-2/5 h-4/5 bg-gray-900 self-center justify-evenly">
+            <Card className=" dark max-w-full w-full h-full">
+              <CardBody className="overflow-hidden">
+                <Tabs
+                  fullWidth
+                  size="lg"
+                  aria-label="Tabs form"
+                  selectedKey={selected}
+                  onSelectionChange={setSelected}
+                >
+                  <Tab key="login" title="Login" className="py-8 text-2xl">
+                    <Login setSelected={setSelected} />
+                  </Tab>
+                  <Tab key="sign-up" title="Register" className="py-8 text-2xl">
+                    <Register setSelected={setSelected} />
+                  </Tab>
+                </Tabs>
+              </CardBody>
+            </Card>
+          </div>
+        )}
+      </Suspense>
     </div>
   );
 }
--- client/src/components/mainbody/page.js
+++ client/src/components/mainbody/page.js
@@ -3,13 +3,27 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { AvatarGroup, Avatar } from "@nextui-org/react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Page = () => {
   return (
-    <div className="dark bg-inherit h-screen text-white relative">
-      <main className="w-full h-full  absolute bg-inherit grid grid-cols-12 grid-rows-8 gap-0">
+    <div className="dark bg-inherit h-screen text-white relative flex justify-center items-center ">
+      <main className="w-11/12 h-full  absolute bg-inherit grid grid-cols-12 grid-rows-8 gap-0">
         <div className="row-span-8 col-span-10  bg-inherit grid grid-cols-10 grid-rows-8">
-          <div className=" row-span-1 bg-amber-950 col-span-10 text-black flex justify-start items-center">
+          <div className=" row-span-1 bg-gray-950 col-span-10 text-black flex justify-start items-center border-1  px-4">
             <AvatarGroup isBordered max={100} total={999} size="lg">
               <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
               <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
@@ -20,12 +34,47 @@ const Page = () => {
             </AvatarGroup>
           </div>
 
-          <div className="bg-blue-600 row-span-7 col-span-2">cat</div>
-          <div className="bg-blue-200 row-span-7 col-span-8 flex-col">
-            <div className="h-1/6">Heading</div>
-            <div className="h-5/6 bg-purple-700 relative text-black">
+          <div className="bg-gray-950 row-span-7 col-span-2 border-1">
+            <Accordion type="single" collapsible className="w-full px-4 py-3">
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Friends</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-2">
+                <AccordionTrigger>Groups</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It comes with default styles that matches the other
+                  components&apos; aesthetic.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-3">
+                <AccordionTrigger>Favourites</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It's animated by default, but you can disable it if you
+                  prefer.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+          <div className="bg-gray-950 row-span-7 col-span-8 flex-col border-1 rounded-lg">
+            <div className="h-1/6 flex justify-between items-center p-8">
+              <div>Heading</div>
+              <div>
+                {" "}
+                <Button
+                  className="ml-2 bg-blue-500 text-white "
+                  variant="secondary"
+                  size="lg"
+                >
+                  Call ?
+                </Button>
+              </div>
+            </div>
+            <div className="h-5/6 relative text-black border-1 pb-2 box-border mx-2 rounded-lg">
               <section className="flex flex-col h-[calc(100%)] bg-[#f7fafc]">
-                <ScrollArea className="flex-1 p-4 space-y-2 bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800">
+                <ScrollArea className="flex-1 p-8 space-y-2 bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800">
                   <div className="flex flex-col gap-4">
                     <p className="bg-white p-2 py-4 rounded-md shadow w-3/6 self-start">
                       Hello, how are you?
@@ -82,7 +131,30 @@ const Page = () => {
             </div>
           </div>
         </div>
-        <div className="row-span-8 col-span-2 bg-green-900">2</div>
+        <div className="row-span-8 col-span-2 bg-gray-950 flex flex-col justify-center items-center border-1 gap-6">
+          <Button
+            className=" px-4 py-7 bg-green-500 text-white  w-5/6"
+            variant="secondary"
+            size="lg"
+          >
+            Find random chat
+          </Button>
+          <Dialog>
+            <DialogTrigger className="border-1 bg-slate-500 px-5 py-3 hover:bg-slate-50 text-black  transition duration-300 ease-in-out rounded-xl  ">
+              {" "}
+              Filter
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                <DialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </div>
       </main>
     </div>
   );
